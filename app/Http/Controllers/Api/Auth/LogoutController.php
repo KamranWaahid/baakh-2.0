dc00d98de4824d7ecad630c42068634a46e76b3e
Kamran Waahid <jamalikamran460@gmail.com>
2026-02-16 03:31:25 +0500

    Fix: Prevent Call to undefined method TransientToken::delete() on logout

--- app/Http/Controllers/Api/Auth/LogoutController.php
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -13,7 +13,9 @@ class LogoutController extends Controller
     public function __invoke(Request $request)
     {
         // Revoke the token that was used to authenticate the current request
-        $request->user()->currentAccessToken()->delete();
+        if (!$request->user()->currentAccessToken() instanceof \Laravel\Sanctum\TransientToken) {
+            $request->user()->currentAccessToken()->delete();
+        }
 
         return response()->json([
             'message' => 'Logged out successfully',
